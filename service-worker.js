238bf6ccd578e82ff31111334c01a384c62af59e
so-si <40689019+so-si@users.noreply.github.com>
2026-08-30 22:49:00 +0900

    Bump PWA cache for daily goal update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flash-cards-pwa-v1';
+const CACHE_NAME = 'flash-cards-pwa-v2';
 const APP_SHELL = ['./', './index.html', './config.js', './manifest.json'];
 
 self.addEventListener('install', event => {
