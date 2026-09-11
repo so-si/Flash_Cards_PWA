7f8aeb9d163345bb843cc2fdb6e1dec36b551e14
so-si <40689019+so-si@users.noreply.github.com>
2026-09-11 12:44:45 +0900

    Bump PWA cache for date fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flash-cards-pwa-v8';
+const CACHE_NAME = 'flash-cards-pwa-v9';
 const APP_SHELL = ['./', './index.html', './config.js', './manifest.json'];
 
 self.addEventListener('install', event => {
