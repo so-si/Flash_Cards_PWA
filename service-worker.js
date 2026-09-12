a7e538721a69e3829469095dd880c0d162898451
so-si <40689019+so-si@users.noreply.github.com>
2026-09-12 17:33:29 +0900

    Bump cache after blank-page syntax fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flash-cards-pwa-v14';
+const CACHE_NAME = 'flash-cards-pwa-v15';
 const APP_SHELL = ['./', './index.html', './config.js', './manifest.json'];
 
 self.addEventListener('install', event => {
