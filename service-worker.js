0565b19d019621cfc4268fff4b12c67e06e7c07a
so-si <40689019+so-si@users.noreply.github.com>
2026-09-20 04:31:37 +0900

    Refresh cache for one-tap reference update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flash-cards-pwa-v19-2';
+const CACHE_NAME = 'flash-cards-pwa-v20';
 const APP_SHELL = ['./', './index.html', './config.js', './manifest.json'];
 
 self.addEventListener('install', event => {
