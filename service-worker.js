455baa8f361f367355156e3e8b2d6fb62d53d6e0
so-si <40689019+so-si@users.noreply.github.com>
2026-09-20 04:18:36 +0900

    Bump PWA cache for full offline reference viewer

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'flash-cards-pwa-v18-pilot3';
-const APP_SHELL = ['./', './index.html', './config.js', './manifest.json', './reference/gyosei/137_行政手続法.html'];
+const CACHE_NAME = 'flash-cards-pwa-v193';
+const APP_SHELL = ['./', './index.html', './config.js', './manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
