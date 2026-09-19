84649754f50048e46c1d373a304b0c030f0db4e6
so-si <40689019+so-si@users.noreply.github.com>
2026-09-20 03:54:53 +0900

    Cache reference index for v18

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'flash-cards-pwa-v16';
-const APP_SHELL = ['./', './index.html', './config.js', './manifest.json'];
+const CACHE_NAME = 'flash-cards-pwa-v18';
+const APP_SHELL = ['./', './index.html', './config.js', './manifest.json', './reference/index.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
