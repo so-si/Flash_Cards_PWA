cc02c4747f17ffb671041a275b2efe237d649c85
so-si <40689019+so-si@users.noreply.github.com>
2026-09-20 04:22:02 +0900

    Refresh cache for reference import fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flash-cards-pwa-v19';
+const CACHE_NAME = 'flash-cards-pwa-v19-1';
 const APP_SHELL = ['./', './index.html', './config.js', './manifest.json'];
 
 self.addEventListener('install', event => {
