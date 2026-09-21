2ff2c8c63a4ac701583bb3d9b8f3aeeeae971043
so-si <40689019+so-si@users.noreply.github.com>
2026-09-21 10:47:44 +0900

    Refresh cache for v20.1 stats fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flash-cards-pwa-v20';
+const CACHE_NAME = 'flash-cards-pwa-v20-1';
 const APP_SHELL = ['./', './index.html', './config.js', './manifest.json'];
 
 self.addEventListener('install', event => {
